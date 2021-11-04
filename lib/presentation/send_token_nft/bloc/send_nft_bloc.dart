9e93123f34c9c26150c6d64874d0078bffdf76ad
huyta <taquanghuy0386356200@gmail.com>
2021-11-04 19:35:26 +0700

    add bloc send nft_screen

--- lib/presentation/send_token_nft/bloc/send_nft_bloc.dart
+++ lib/presentation/send_token_nft/bloc/send_nft_bloc.dart
@@ -3,9 +3,18 @@ import 'package:flutter/services.dart';
 import '../../../main.dart';
 
 class SendNftBloc {
+  String walletAddress = '';
+  String receiveAddress = '';
+  int? nftID;
+  String password = '';
+
   Future<dynamic> nativeMethodCallBackTrustWallet(MethodCall methodCall) async {
     switch (methodCall.method) {
       case "checkPasswordCallback":
+        walletAddress = await methodCall.arguments['walletAddress'];
+        receiveAddress = await methodCall.arguments['receiveAddress'];
+        password = await methodCall.arguments['password'];
+        nftID = await methodCall.arguments['nftID'];
         break;
 
       default:
@@ -13,11 +22,12 @@ class SendNftBloc {
     }
   }
 
-  Future<void> sendNftWallet(
-      {required String walletAddress,
-      required String receiveAddress,
-      required int nftID,
-      String? password}) async {
+  Future<void> sendNftWallet({
+    required String walletAddress,
+    required String receiveAddress,
+    required int nftID,
+    String? password,
+  }) async {
     try {
       final data = {
         'password': password,
