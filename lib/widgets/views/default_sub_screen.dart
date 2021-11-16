bcc277226c1f05bc7177a6c8a7f3753b0e44d01f
DESKTOP-88FAOV3\EdsoLabs <>
2021-11-16 08:33:21 +0700

    "modify padding within text input form seed phrase, bg color of detail transition and size of it"

--- lib/widgets/views/default_sub_screen.dart
+++ lib/widgets/views/default_sub_screen.dart
@@ -9,71 +9,68 @@ class DefaultSubScreen extends StatelessWidget {
   final String title;
   final Widget mainWidget;
 
-  const DefaultSubScreen(
-      {Key? key, required this.title, required this.mainWidget})
-      : super(key: key);
+  const DefaultSubScreen({
+    Key? key,
+    required this.title,
+    required this.mainWidget,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Container(
-          color: Colors.black,
-          child: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(30.h),
-                topRight: Radius.circular(30.h),
-              ),
-              gradient: const LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: listBackgroundColor,
-              ),
+    return Container(
+      height: 764.h,
+      width: 375.w,
+      decoration: BoxDecoration(
+        color: AppTheme.getInstance().bgBtsColor(),
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
+        ),
+      ),
+      child: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.only(
+              top: 16.h,
+              left: 16.h,
+              right: 16.h,
+              bottom: 20.h,
             ),
-            child: Column(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Container(
-                  padding: EdgeInsets.only(
-                      top: 16.h, left: 16.h, right: 16.h, bottom: 20.h,),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SizedBox(
-                        height: 28.h,
-                        width: 28.h,
-                        child: IconButton(
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          icon: Image.asset(ImageAssets.ic_back),
-                        ),
-                      ),
-                      Expanded(
-                        child: Center(
-                          child: Text(
-                            title,
-                            style: titleText(
-                              color: AppTheme.getInstance().textThemeColor(),
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 28.h,
-                        width: 28.h,
-                      )
-                    ],
+                SizedBox(
+                  height: 28.h,
+                  width: 28.h,
+                  child: IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: Image.asset(ImageAssets.ic_back),
                   ),
                 ),
-                Divider(
-                  color: AppTheme.getInstance().divideColor(),
+                Expanded(
+                  child: Center(
+                    child: Text(
+                      title,
+                      style: titleText(
+                        color: AppTheme.getInstance().textThemeColor(),
+                      ),
+                    ),
+                  ),
                 ),
-                Expanded(child: mainWidget),
+                SizedBox(
+                  height: 28.h,
+                  width: 28.h,
+                )
               ],
             ),
           ),
-        ),
+          Divider(
+            color: AppTheme.getInstance().divideColor(),
+          ),
+          Expanded(child: mainWidget),
+        ],
       ),
     );
   }
