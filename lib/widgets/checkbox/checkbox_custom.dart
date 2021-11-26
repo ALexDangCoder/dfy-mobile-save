057aef1861b7b5d588f45d25a3f0b7aeb61e440f
DESKTOP-88FAOV3\EdsoLabs <>
2021-11-26 09:55:41 +0700

    fix checkbox_custom

--- lib/widgets/checkbox/checkbox_custom.dart
+++ lib/widgets/checkbox/checkbox_custom.dart
@@ -19,39 +19,50 @@ class CheckBoxCustom extends StatelessWidget {
     return Container(
       margin: EdgeInsets.only(right: 16.w, left: 16.w),
       child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          StreamBuilder(
-            stream: bLocCreateSeedPhrase.isCheckBox1,
-            builder: (context, AsyncSnapshot<bool> snapshot) {
-              return Transform.scale(
-                scale: 1.sp,
-                child: Checkbox(
-                  fillColor: MaterialStateProperty.all(
-                    AppTheme.getInstance().fillColor(),
-                  ),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(6.r),
-                  ),
-                  value: snapshot.data ?? false,
-                  onChanged: (value) {
-                    bLocCreateSeedPhrase.isCheckBox1.sink.add(true);
-                    if (snapshot.data ?? false) {
-                      bLocCreateSeedPhrase.isCheckBox1.sink.add(false);
-                    }
+          SizedBox(
+            width: 24.w,
+            height: 24.h,
+            child: StreamBuilder(
+              stream: bLocCreateSeedPhrase.isCheckBox1,
+              builder: (context, AsyncSnapshot<bool> snapshot) {
+                return Transform.scale(
+                  scale: 1.sp,
+                  child: Checkbox(
+                    fillColor: MaterialStateProperty.all(
+                      AppTheme.getInstance().fillColor(),
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(6.r),
+                    ),
+                    value: snapshot.data ?? false,
+                    onChanged: (value) {
+                      bLocCreateSeedPhrase.isCheckBox1.sink.add(true);
+                      if (snapshot.data ?? false) {
+                        bLocCreateSeedPhrase.isCheckBox1.sink.add(false);
+                      }
 
-                    bLocCreateSeedPhrase.isButton();
-                  },
-                  activeColor: AppTheme.getInstance().fillColor(),
-                ),
-              );
-            },
+                      bLocCreateSeedPhrase.isButton();
+                    },
+                    activeColor: AppTheme.getInstance().fillColor(),
+                  ),
+                );
+              },
+            ),
           ),
           Expanded(
-            child: Text(
-              title,
-              style: textNormal(
-                AppTheme.getInstance().textThemeColor(),
-                14.sp,
+            child: Container(
+              margin: EdgeInsets.only(
+                left: 4.w,
+                top: 3.h,
+              ),
+              child: Text(
+                title,
+                style: textNormal(
+                  AppTheme.getInstance().textThemeColor(),
+                  14.sp,
+                ),
               ),
             ),
           )
