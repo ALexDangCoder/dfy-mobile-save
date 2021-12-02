fad3ad836fd3adf06ffb2dfd749039221a66f9db
NguyenHuuNghia <>
2021-12-02 17:27:51 +0700

    add web3

--- lib/data/web3/web3_utils.dart
+++ lib/data/web3/web3_utils.dart
@@ -1,6 +1,8 @@
 import 'package:Dfy/data/web3/model/nft_info_model.dart';
-
-import 'model/token_info_model.dart';
+import 'package:Dfy/data/web3/model/token_info_model.dart';
+import 'package:Dfy/utils/constants/app_constants.dart';
+import 'package:http/http.dart';
+import 'package:web3dart/web3dart.dart';
 
 class Web3Utils {
   Web3Utils._privateConstructor();
@@ -9,11 +11,20 @@ class Web3Utils {
 
   factory Web3Utils() => _shared;
 
+  //client
+  final client = Web3Client(rpcURL, Client());
+
   //Token
 
   //Token info
   TokenInfoModel getTokenInfo({required String contractAddress}) {
     return TokenInfoModel();
+  }
+
+  //get balance of BNB from an address
+  Future<double> getBalanceOfBnb({required String ofAddress}) async {
+    final amount = await client.getBalance(EthereumAddress.fromHex(ofAddress));
+    return amount.getInWei / BigInt.from(10).pow(18);
   }
 
   //get balance of an address
@@ -22,12 +33,25 @@ class Web3Utils {
   }
 
   //get gas price
-  Future<int> getGasPrice() async {
-    return 10;
+  Future<double> getGasPrice() async {
+    final amount = await client.getGasPrice();
+    return amount.getInWei / BigInt.from(10).pow(9);
   }
 
-  Future<int> getEstimateGasPrice() async {
-    return 0;
+  Future<double> getEstimateGasPrice({
+    required String from,
+    required String to,
+    required double value,
+  }) async {
+    final amount = await client.estimateGas(
+      sender: EthereumAddress.fromHex(from),
+      to: EthereumAddress.fromHex(to),
+      value: EtherAmount.fromUnitAndValue(
+        EtherUnit.gwei,
+        (value * 1000000000).toInt(),
+      ),
+    );
+    return amount / BigInt.from(10).pow(9);
   }
 
   //Token detail
@@ -40,11 +64,11 @@ class Web3Utils {
   }
 
   //NFT detail
-  Future<NftInfoModel> getNftDetail({
+  Future<NftInfo> getNftDetail({
     required String contractAddress,
     required String walletAddress,
     String? password,
   }) async {
-    return NftInfoModel();
+    return NftInfo();
   }
 }
