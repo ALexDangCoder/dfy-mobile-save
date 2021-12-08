bd85dad8635fb00edbca7f7dbb58828b9aeb5623
huyta <taquanghuy0386356200@gmail.com>
2021-12-08 20:02:38 +0700

    push json to importNftlist

--- lib/data/web3/model/nft_info_model.dart
+++ lib/data/web3/model/nft_info_model.dart
@@ -19,6 +19,7 @@ class NftInfo {
     this.standard,
     this.blockchain,
   });
+
   NftInfo.fromJson(Map<String, dynamic> json) {
     const url = 'https://defiforyou.mypinata.cloud/ipfs/';
     collectionId = json['collection_id'];
@@ -27,35 +28,20 @@ class NftInfo {
     name = json['name'];
   }
 
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    data['contract'] = contract;
-    data['collectionId'] = collectionId;
-    data['name'] = name;
-    data['id'] = id;
-    data['img'] = img;
-    data['link'] = link;
-    data['description'] = description;
-    data['name'] = name;
-    data['standard'] = standard;
-    data['blockchain'] = blockchain;
-    return data;
-  }
-
   Map<String, dynamic> saveToJson({required String walletAddress}) {
     final Map<String, dynamic> data = <String, dynamic>{};
     data['walletAddress'] = walletAddress;
-    data['collectionAddress'] = contract;
-    data['nftAddress'] = contract;
+    data['collectionAddress'] = contract ?? '';
+    data['nftAddress'] = contract ?? '';
     data['collectionId'] = collectionId;
     data['nftName'] = name;
     data['nftID'] = int.parse(id ?? '-1');
     data['iconNFT'] = img;
-    data['link'] = link;
+    data['link'] = link ?? '';
     data['description'] = description;
     data['name'] = name;
-    data['standard'] = standard;
-    data['blockchain'] = blockchain;
+    data['standard'] = standard ?? '';
+    data['blockchain'] = blockchain ?? '';
     return data;
   }
 }
--- lib/presentation/wallet/bloc/wallet_cubit.dart
+++ lib/presentation/wallet/bloc/wallet_cubit.dart
@@ -512,7 +512,7 @@ class WalletCubit extends BaseCubit<WalletState> {
     } else {
       // If the server did not return a 200 OK response,
       // then throw an exception.
-      throw Exception('Failed to load album');
+      throw Exception('Failed to load Collection');
     }
   }
 
