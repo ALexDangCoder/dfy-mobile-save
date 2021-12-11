222cb7e36fc16e5263b50ae8d4c55ed57dde43f5
taquanghuy02032000 <taquanghuy0386356200@gmail.com>
2021-12-11 23:09:31 +0700

    fix ui show prkey_seedpharse

--- lib/presentation/show_pw_prvkey_seedpharse/ui/confirm_pw_prvkey_seedpharse.dart
+++ lib/presentation/show_pw_prvkey_seedpharse/ui/confirm_pw_prvkey_seedpharse.dart
@@ -66,10 +66,6 @@ class _ConfirmPWShowPRVSeedPhrState extends State<ConfirmPWShowPRVSeedPhr> {
               return GestureDetector(
                 onTap: () {
                   if (snapshot.data ?? false) {
-                    widget.cubit.checkValidate(
-                      txtController.text,
-                      rightPW: password,
-                    );
                     if (widget.cubit.isValidPW) {
                       widget.cubit.getListPrivateKeyAndSeedPhrase(
                         password: '123456aA@',
@@ -153,10 +149,6 @@ class _ConfirmPWShowPRVSeedPhrState extends State<ConfirmPWShowPRVSeedPhr> {
     return Container(
       height: 64.h,
       width: 343.w,
-      padding: EdgeInsets.only(
-        top: 12.h,
-        bottom: 12.h,
-      ),
       decoration: BoxDecoration(
         borderRadius: const BorderRadius.all(
           Radius.circular(20),
@@ -166,50 +158,56 @@ class _ConfirmPWShowPRVSeedPhrState extends State<ConfirmPWShowPRVSeedPhr> {
       child: StreamBuilder<bool>(
         stream: widget.cubit.showPWStream,
         builder: (context, snapshot) {
-          return TextFormField(
-            onChanged: (value) {
-              widget.cubit.isEnableButton(
-                value: value,
-              );
-            },
-            style: textNormal(
-              Colors.white,
-              16,
-            ),
-            obscureText: snapshot.data ?? true,
-            cursorColor: Colors.white,
-            controller: controller,
-            decoration: InputDecoration(
-              hintText: hintText,
-              hintStyle: textNormal(
-                Colors.grey,
-                14,
+          return Center(
+            child: TextFormField(
+              onChanged: (value) {
+                widget.cubit.checkValidate(
+                  value,
+                );
+                widget.cubit.isEnableButton(
+                  value: value,
+                );
+              },
+              style: textNormal(
+                Colors.white,
+                16,
               ),
-              suffixIcon: InkWell(
-                onTap: () {
-                  if (index == 0) {
-                    index = 1;
-                    widget.cubit.showPW(0);
-                  } else {
-                    index = 0;
-                    widget.cubit.showPW(1);
-                  }
-                },
-                child: snapshot.data ?? false
-                    ? const ImageIcon(
-                        AssetImage(ImageAssets.ic_show),
-                        color: Colors.grey,
-                      )
-                    : const ImageIcon(
-                        AssetImage(ImageAssets.ic_hide),
-                        color: Colors.grey,
-                      ),
+              textAlignVertical: TextAlignVertical.center,
+              obscureText: snapshot.data ?? true,
+              cursorColor: Colors.white,
+              controller: controller,
+              decoration: InputDecoration(
+                hintText: hintText,
+                hintStyle: textNormal(
+                  Colors.grey,
+                  14,
+                ),
+                suffixIcon: InkWell(
+                  onTap: () {
+                    if (index == 0) {
+                      index = 1;
+                      widget.cubit.showPW(0);
+                    } else {
+                      index = 0;
+                      widget.cubit.showPW(1);
+                    }
+                  },
+                  child: snapshot.data ?? false
+                      ? const ImageIcon(
+                          AssetImage(ImageAssets.ic_show),
+                          color: Colors.grey,
+                        )
+                      : const ImageIcon(
+                          AssetImage(ImageAssets.ic_hide),
+                          color: Colors.grey,
+                        ),
+                ),
+                prefixIcon: const ImageIcon(
+                  AssetImage(ImageAssets.ic_lock),
+                  color: Colors.white,
+                ),
+                border: InputBorder.none,
               ),
-              prefixIcon: const ImageIcon(
-                AssetImage(ImageAssets.ic_lock),
-                color: Colors.white,
-              ),
-              border: InputBorder.none,
             ),
           );
         },
