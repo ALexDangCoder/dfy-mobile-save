3f34da2225ce9b2c19f9ad4c5fe40063965b31ce
taquanghuy02032000 <taquanghuy0386356200@gmail.com>
2021-12-23 13:45:57 +0700

    fix bug bda log

--- lib/presentation/send_token_nft/ui/send_token/send_token.dart
+++ lib/presentation/send_token_nft/ui/send_token/send_token.dart
@@ -331,15 +331,32 @@ class _SendTokenState extends State<SendToken> {
                       },
                       child: Padding(
                         padding: EdgeInsets.only(
-                          top: 15.h,
+                          // top: 15.h,
                           right: 20.w,
                         ),
-                        child: Text(
-                          S.current.max,
-                          style: textNormal(
-                            const Color.fromRGBO(228, 172, 26, 1),
-                            16,
-                          ).copyWith(fontWeight: FontWeight.w600),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            SizedBox(
+                              height: 20.h,
+                              width: 20.h,
+                              child: CircleAvatar(
+                                radius: 30.0.r,
+                                backgroundImage:
+                                NetworkImage(widget.modelToken.iconToken),
+                                backgroundColor: Colors.transparent,
+                              ),
+                            ),
+                            spaceW5,
+                            Text(
+                              S.current.max,
+                              style: textNormal(
+                                const Color.fromRGBO(228, 172, 26, 1),
+                                16,
+                              ).copyWith(fontWeight: FontWeight.w600),
+                            ),
+                          ],
                         ),
                       ),
                     )
