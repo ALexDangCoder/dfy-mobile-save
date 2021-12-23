acf952a6fe9d3513ffcfe4b9bc88adb99b1baddb
Doanhkull511a <doanhkull511a@gmail.com>
2021-12-23 15:57:47 +0700

    Fix ui import token

--- lib/presentation/import_token_nft/ui/import_token.dart
+++ lib/presentation/import_token_nft/ui/import_token.dart
@@ -29,143 +29,141 @@ class ImportTokenScreen extends StatelessWidget {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: Colors.transparent,
-      body: ListView(
-        children: [
-          spaceH8,
-          DefaultTabController(
-            length: 2,
-            child: Container(
-              height: 764.h,
-              width: 375.w,
-              decoration: BoxDecoration(
-                color: AppTheme.getInstance().bgBtsColor(),
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30.r),
-                  topRight: Radius.circular(30.r),
-                ),
+      body:Align(
+        alignment: Alignment.bottomCenter,
+        child:   DefaultTabController(
+          length: 2,
+          child: Container(
+            height: 764.h,
+            width: 375.w,
+            decoration: BoxDecoration(
+              color: AppTheme.getInstance().bgBtsColor(),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30.r),
+                topRight: Radius.circular(30.r),
               ),
-              child: Column(
-                children: [
-                  Container(
-                    width: 343.w,
-                    height: 28.h,
-                    margin: EdgeInsets.only(
-                      left: 16.w,
-                      top: 16.h,
-                      right: 16.w,
-                      bottom: 20.h,
+            ),
+            child: Column(
+              children: [
+                Container(
+                  width: 343.w,
+                  height: 28.h,
+                  margin: EdgeInsets.only(
+                    left: 16.w,
+                    top: 16.h,
+                    right: 16.w,
+                    bottom: 20.h,
+                  ),
+                  child: Row(
+                    children: [
+                      GestureDetector(
+                        child: Container(
+                          margin: EdgeInsets.only(right: 10.w, left: 10.w),
+                          child: Image.asset(
+                            ImageAssets.ic_back,
+                            width: 20.w,
+                            height: 20.h,
+                          ),
+                        ),
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(left: 70.w),
+                        child: Text(
+                          S.current.import_token,
+                          style: textNormalCustom(
+                            null,
+                            20,
+                            FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                line,
+                spaceH12,
+                Center(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(12.r),
+                      ),
+                      color: backgroundBottomSheetColor,
                     ),
-                    child: Row(
-                      children: [
-                        GestureDetector(
-                          child: Container(
-                            margin: EdgeInsets.only(right: 10.w, left: 10.w),
-                            child: Image.asset(
-                              ImageAssets.ic_back,
-                              width: 20.w,
-                              height: 20.h,
+                    height: 35.h,
+                    width: 253.w,
+                    child: TabBar(
+                      tabs: [
+                        Tab(
+                          child: FittedBox(
+                            child: Text(
+                              S.current.enter_token,
+                              style: textNormalCustom(
+                                null,
+                                14,
+                                FontWeight.bold,
+                              ),
                             ),
                           ),
-                          onTap: () {
-                            Navigator.pop(context);
-                          },
                         ),
-                        Container(
-                          margin: EdgeInsets.only(left: 70.w),
-                          child: Text(
-                            S.current.import_token,
-                            style: textNormalCustom(
-                              null,
-                              20,
-                              FontWeight.bold,
+                        Tab(
+                          child: FittedBox(
+                            child: Text(
+                              S.current.choose_token,
+                              style: textNormalCustom(
+                                null,
+                                14,
+                                FontWeight.bold,
+                              ),
                             ),
                           ),
                         ),
                       ],
-                    ),
-                  ),
-                  line,
-                  spaceH12,
-                  Center(
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(12.r),
-                        ),
-                        color: backgroundBottomSheetColor,
-                      ),
-                      height: 35.h,
-                      width: 253.w,
-                      child: TabBar(
-                        tabs: [
-                          Tab(
-                            child: FittedBox(
-                              child: Text(
-                                S.current.enter_token,
-                                style: textNormalCustom(
-                                  null,
-                                  14,
-                                  FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                          ),
-                          Tab(
-                            child: FittedBox(
-                              child: Text(
-                                S.current.choose_token,
-                                style: textNormalCustom(
-                                  null,
-                                  14,
-                                  FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                        labelColor: AppTheme.getInstance().whiteColor(),
-                        unselectedLabelColor:
-                            AppTheme.getInstance().whiteColor(),
-                        indicator: RectangularIndicator(
-                          bottomLeftRadius: 10.r,
-                          bottomRightRadius: 10.r,
-                          topLeftRadius: 10.r,
-                          topRightRadius: 10.r,
-                          color: formColor,
-                          horizontalPadding: 3.w,
-                          verticalPadding: 3.h,
-                        ),
+                      labelColor: AppTheme.getInstance().whiteColor(),
+                      unselectedLabelColor:
+                      AppTheme.getInstance().whiteColor(),
+                      indicator: RectangularIndicator(
+                        bottomLeftRadius: 10.r,
+                        bottomRightRadius: 10.r,
+                        topLeftRadius: 10.r,
+                        topRightRadius: 10.r,
+                        color: formColor,
+                        horizontalPadding: 3.w,
+                        verticalPadding: 3.h,
                       ),
                     ),
                   ),
-                  spaceH12,
-                  line,
-                  Expanded(
-                    child: GestureDetector(
-                      onTap: () {
-                        final FocusScopeNode currentFocus =
-                            FocusScope.of(context);
-                        if (!currentFocus.hasPrimaryFocus) {
-                          currentFocus.unfocus();
-                        }
-                      },
-                      child: TabBarView(
-                        children: [
-                          EnterAddress(
-                            bloc: bloc,
-                            addressWallet: addressWallet,
-                          ),
-                          ChooseToken(bloc: bloc),
-                        ],
-                      ),
+                ),
+                spaceH12,
+                line,
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      final FocusScopeNode currentFocus =
+                      FocusScope.of(context);
+                      if (!currentFocus.hasPrimaryFocus) {
+                        currentFocus.unfocus();
+                      }
+                    },
+                    child: TabBarView(
+                      children: [
+                        EnterAddress(
+                          bloc: bloc,
+                          addressWallet: addressWallet,
+                        ),
+                        ChooseToken(bloc: bloc),
+                      ],
                     ),
-                  )
-                ],
-              ),
+                  ),
+                )
+              ],
             ),
           ),
-        ],
-      ),
+        ),
+      )
     );
   }
 }
