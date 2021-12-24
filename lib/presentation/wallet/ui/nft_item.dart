ed2f65403d7e5857fffd46a04d479daefffa8737
taquanghuy02032000 <taquanghuy0386356200@gmail.com>
2021-12-24 16:08:50 +0700

    fix import duplicate nft

--- lib/presentation/wallet/ui/nft_item.dart
+++ lib/presentation/wallet/ui/nft_item.dart
@@ -98,7 +98,7 @@ class _NFTItemState extends State<NFTItem> {
                       width: 8.w,
                     ),
                     ConstrainedBox(
-                      constraints: BoxConstraints(maxWidth: 248.w),
+                      constraints: BoxConstraints(maxWidth: 240.w),
                       child: Text(
                         widget.collectionShow.name!,
                         style: textNormalCustom(
