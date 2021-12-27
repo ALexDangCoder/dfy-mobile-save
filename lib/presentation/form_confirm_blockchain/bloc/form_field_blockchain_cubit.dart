7425461fece3e0f2adb51f2eec8173a0a3b87e58
namnguyenthanh <nam.nt0910@gmail.com>
2021-12-27 17:00:46 +0700

    "get history ok"

--- lib/presentation/form_confirm_blockchain/bloc/form_field_blockchain_cubit.dart
+++ lib/presentation/form_confirm_blockchain/bloc/form_field_blockchain_cubit.dart
@@ -241,7 +241,7 @@ class FormFieldBlockchainCubit extends Cubit<FormFieldBlockchainState> {
             status: status,
             gasFee: gasFee,
             dateTime: dateTime,
-            txhID: txHashToken,
+            txhID: txHashNft,
             toAddress: toAddress,
             nonce: nonce,
             name: name,
