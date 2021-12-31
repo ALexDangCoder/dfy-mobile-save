186d30ef585f2c8a3d4d4ef6065234acfd77010f
Doanhkull511a <doanhkull511a@gmail.com>
2021-12-31 14:21:09 +0700

    update MODEL COLLECTION

---
+++ lib/domain/model/market_place/outstanding_collection_model.dart
@@ -0,0 +1,23 @@
+class OutstandingCollection {
+  String? id;
+  int? position;
+  String? name;
+  String? itemId;
+  String? avatarCid;
+  String? coverCid;
+  String? featureCid;
+  int? totalNft;
+  int? nftOwnerCount;
+
+  OutstandingCollection({
+    this.id,
+    this.position,
+    this.name,
+    this.itemId,
+    this.avatarCid,
+    this.coverCid,
+    this.featureCid,
+    this.totalNft,
+    this.nftOwnerCount,
+  });
+}
