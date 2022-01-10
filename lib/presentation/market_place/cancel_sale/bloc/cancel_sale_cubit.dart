a030a64313e25d9852668cb13d3496498fdacb32
Duy <vund.0709@gmail.com>
2022-01-10 15:22:12 +0700

    Get fee từ web3

--- lib/presentation/market_place/cancel_sale/bloc/cancel_sale_cubit.dart
+++ lib/presentation/market_place/cancel_sale/bloc/cancel_sale_cubit.dart
@@ -1,16 +1,31 @@
+import 'dart:developer';
+
+import 'package:Dfy/config/base/base_cubit.dart';
 import 'package:Dfy/data/web3/web3_utils.dart';
 import 'package:Dfy/domain/model/detail_item_approve.dart';
 import 'package:Dfy/generated/l10n.dart';
-import 'package:bloc/bloc.dart';
+import 'package:Dfy/main.dart';
+import 'package:Dfy/utils/constants/app_constants.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
 import 'package:meta/meta.dart';
+import 'package:rxdart/rxdart.dart';
 
 part 'cancel_sale_state.dart';
 
-class CancelSaleCubit extends Cubit<CancelSaleState> {
+class CancelSaleCubit extends BaseCubit<CancelSaleState> {
   CancelSaleCubit({required this.ntfValue, required this.quantity})
       : super(CancelSaleInitial());
   final String ntfValue;
   final String quantity;
+
+  String transaction = '';
+  String dataString = '';
+  BehaviorSubject<double> gasLimitSubject = BehaviorSubject.seeded(0);
+
+  Stream<double> get gasLimitStream => gasLimitSubject.stream;
+
+  Web3Utils web3utils = Web3Utils();
 
   List<DetailItemApproveModel> initListApprove() {
     List<DetailItemApproveModel> listApprove = [
@@ -20,7 +35,68 @@ class CancelSaleCubit extends Cubit<CancelSaleState> {
     return listApprove;
   }
 
-  Future<void> getEstimateFromWeb3()async {
-    Web3Utils web3utils = Web3Utils();
+  //get limit gas
+  Future<void> getGasLimit(
+      {required BuildContext context,
+      required String orderId,
+      required String walletAddress}) async {
+    //a19d9f27-e023-4824-8a6e-23a9566fb733
+    //get dataString
+    dataString = await web3utils.getCancelListingData(
+      contractAddress: nft_sales_address_dev2,
+      orderId: orderId,
+      context: context,
+    );
+    //get gas limit by  data
+    double gasLimit = double.parse(
+      await web3utils.getGasLimitByData(
+        from: walletAddress,
+        toContractAddress: nft_sales_address_dev2,
+        dataString: dataString,
+      ),
+    );
+    gasLimitSubject.sink.add(gasLimit);
+  }
+
+  //ky giao dich
+  Future<void> signTransactionWithData(
+      {required String walletAddress,
+      required String nonce,
+      required String chainId,
+      required String gasPrice,
+      required String gasLimit,
+      required String withData}) async {
+    try {
+      final TransactionCountResponse transaction =
+          await web3utils.getTransactionCount(address: walletAddress);
+      if (!transaction.isSuccess) {
+        return;
+      }
+      final data = {
+        'walletAddress': walletAddress,
+        'contractAddress': nft_sales_address_dev2,
+        'nonce': transaction.count.toString(),
+        'chainId': String,
+        'gasPrice': String,
+        'gasLimit': String,
+        'withData': withData,
+      };
+      await trustWalletChannel.invokeMethod('signTransactionWithData', data);
+    } on PlatformException catch (e) {
+      //
+    }
+  }
+
+  Future<dynamic> nativeMethodCallBackTrustWallet(MethodCall methodCall) async {
+    if (methodCall.method == 'signTransactionWithDataCallback') {
+      transaction = await methodCall.arguments;
+    }
+  }
+
+  //cancel sale:
+  Future<Map<String, dynamic>> cancelSale() async {
+    final Map<String, dynamic> res =
+        await web3utils.sendRawTransaction(transaction: transaction);
+    return res;
   }
 }
--- lib/presentation/market_place/cancel_sale/ui/cancel_sale.dart
+++ lib/presentation/market_place/cancel_sale/ui/cancel_sale.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:Dfy/config/resources/styles.dart';
 import 'package:Dfy/config/themes/app_theme.dart';
 import 'package:Dfy/generated/l10n.dart';
@@ -9,7 +11,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class CancelSale extends StatelessWidget {
+class CancelSale extends StatefulWidget {
   final CancelSaleCubit cubit;
 
   const CancelSale({
@@ -18,49 +20,72 @@ class CancelSale extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<CancelSale> createState() => _CancelSaleState();
+}
+
+class _CancelSaleState extends State<CancelSale> {
+
+  @override
+  void initState() {
+      widget.cubit.getGasLimit(
+      walletAddress: '0x39ee4c28E09ce6d908643dDdeeAeEF2341138eBB',
+      context: context,
+      orderId: '80',
+    );
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Approve(
-      listDetail: cubit.initListApprove(),
-      action: () {},
-      title: S.current.cancel_sale,
-      header: Container(
-        padding: EdgeInsets.only(
-          top: 16.h,
-          bottom: 20.h,
-        ),
-        alignment: Alignment.centerLeft,
-        child: Text(
-          S.current.cancel_sale_info,
-          style: textNormal(
-            AppTheme.getInstance().whiteColor(),
-            16.sp,
-          ).copyWith(
-            fontWeight: FontWeight.w600,
-          ),
-        ),
-      ),
-      approve: () {},
-      warning: Row(
-        children: [
-          sizedSvgImage(
-              w: 16.67.w, h: 16.67.h, image: ImageAssets.ic_warning_canel),
-          SizedBox(
-            width: 5.w,
-          ),
-          Expanded(
+    return StreamBuilder<double>(
+      stream: widget.cubit.gasLimitStream,
+      builder: (context, snapshot) {
+        log('>>>>>>>>>'+snapshot.data.toString());
+        return Approve(
+          listDetail: widget.cubit.initListApprove(),
+          action: () {},
+          title: S.current.cancel_sale,
+          header: Container(
+            padding: EdgeInsets.only(
+              top: 16.h,
+              bottom: 20.h,
+            ),
+            alignment: Alignment.centerLeft,
             child: Text(
-              S.current.customer_cannot,
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
+              S.current.cancel_sale_info,
               style: textNormal(
-                AppTheme.getInstance().currencyDetailTokenColor(),
-                14.sp,
+                AppTheme.getInstance().whiteColor(),
+                16.sp,
+              ).copyWith(
+                fontWeight: FontWeight.w600,
               ),
             ),
           ),
-        ],
-      ),
-      textActiveButton: S.current.cancel_sale,
+          approve: () {},
+          warning: Row(
+            children: [
+              sizedSvgImage(
+                  w: 16.67.w, h: 16.67.h, image: ImageAssets.ic_warning_canel),
+              SizedBox(
+                width: 5.w,
+              ),
+              Expanded(
+                child: Text(
+                  S.current.customer_cannot,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: textNormal(
+                    AppTheme.getInstance().currencyDetailTokenColor(),
+                    14.sp,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          textActiveButton: S.current.cancel_sale,
+          gasLimit: widget.cubit.gasLimitSubject.value,
+        );
+      }
     );
   }
 }
