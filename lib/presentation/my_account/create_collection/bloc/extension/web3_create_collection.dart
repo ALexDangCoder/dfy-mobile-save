2f480fa0f6aa3640ff35e3c0f47f9f478e450ccc
haipn5991 <mraugust95@gmail.com>
2022-01-25 14:39:42 +0700

    add constant value

--- lib/presentation/my_account/create_collection/bloc/extension/web3_create_collection.dart
+++ lib/presentation/my_account/create_collection/bloc/extension/web3_create_collection.dart
@@ -1,26 +1,28 @@
 import 'package:Dfy/presentation/my_account/create_collection/bloc/create_collection_cubit.dart';
 import 'package:Dfy/presentation/my_account/create_collection/bloc/extension/ipfs_gen_url.dart';
+import 'package:Dfy/utils/constants/api_constants.dart';
 import 'package:Dfy/utils/constants/app_constants.dart';
+import 'package:Dfy/utils/extensions/map_extension.dart';
 import 'package:flutter/material.dart';
 
 extension Web3Call on CreateCollectionCubit {
   Future<void> sendDataWeb3(BuildContext context) async {
     showLoading();
-    const prefixURL = 'https://marketplace.defiforyou.uk/';
+    const prefixURL = ApiConstants.PREFIX_CUSTOM_URL;
     await generateRandomURL();
     final Map<String, dynamic> jsonBody = {
       'external_link':
-    'https://defiforyou.mypinata.cloud/ipfs/${cidMap['avatar_cid']}',
-    'feature_cid': cidMap['feature_cid'],
-    'image': 'https://defiforyou.mypinata.cloud/ipfs/${cidMap['avatar_cid']}',
-    'name': collectionName,
-    'custom_url': '$prefixURL$customUrl',
-    'avatar_cid': cidMap['avatar_cid'],
-    'category': categoryId,
-    'cover_cid': cidMap['cover_cid'],
-    'social_links': socialLinkMap.toString(),
+          ApiConstants.URL_BASE + cidMap.getStringValue('avatar_cid'),
+      'feature_cid': cidMap.getStringValue('feature_cid'),
+      'image': ApiConstants.URL_BASE + (cidMap.getStringValue('avatar_cid')),
+      'name': collectionName,
+      'custom_url': '$prefixURL$customUrl',
+      'avatar_cid': cidMap['avatar_cid'],
+      'category': categoryId,
+      'cover_cid': cidMap['cover_cid'],
+      'social_links': socialLinkMap.toString(),
     };
-    collectionIPFS = await ipfsService.pinJsonToIPFS(bodyMap : jsonBody);
+    collectionIPFS = await ipfsService.pinJsonToIPFS(bodyMap: jsonBody);
     if (collectionType == 0) {
       transactionData = await web3utils.getCreateCollectionData(
         contractAddress: nft_factory_dev2,
