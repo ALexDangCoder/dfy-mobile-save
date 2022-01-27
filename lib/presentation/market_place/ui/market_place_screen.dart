208abdd4012e381510c463fb448fe0d5c590c7e5
Doanhkull511a <doanhkull511a@gmail.com>
2022-01-27 15:52:33 +0700

    COMMIT MERGE

--- lib/presentation/market_place/ui/market_place_screen.dart
+++ lib/presentation/market_place/ui/market_place_screen.dart
@@ -16,6 +16,8 @@ import 'package:Dfy/presentation/market_place/ui/components_list_nft_categories/
 import 'package:Dfy/presentation/my_account/create_collection/bloc/create_collection_cubit.dart';
 import 'package:Dfy/presentation/my_account/create_collection/ui/create_collection_screen.dart';
 import 'package:Dfy/presentation/market_place/ui/header.dart';
+import 'package:Dfy/presentation/my_account/create_nft/bloc/create_nft_cubit.dart';
+import 'package:Dfy/presentation/my_account/create_nft/ui/create_nft_screen.dart';
 import 'package:Dfy/widgets/floating_button/ui/float_btn_add.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -29,7 +31,8 @@ class MarketPlaceScreen extends StatefulWidget {
   _MarketPlaceState createState() => _MarketPlaceState();
 }
 
-class _MarketPlaceState extends State<MarketPlaceScreen>  with AutomaticKeepAliveClientMixin<MarketPlaceScreen>{
+class _MarketPlaceState extends State<MarketPlaceScreen>
+    with AutomaticKeepAliveClientMixin<MarketPlaceScreen> {
   late MarketplaceCubit cubit;
 
   @override
@@ -183,12 +186,15 @@ class _MarketPlaceState extends State<MarketPlaceScreen>  with AutomaticKeepAliv
                       );
                     },
                     nftCallBack: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => ListBookEvaluation(),
+                      showDialog(
+                        context: context,
+                        builder: (_) => ConnectWalletDialog(
+                          navigationTo: CreateNFTScreen(
+                            cubit: CreateNftCubit(),
+                          ),
+                          isRequireLoginEmail: false,
                         ),
-                      ); //todo tesst nhowf
+                      );
                     },
                   ),
                 ),
