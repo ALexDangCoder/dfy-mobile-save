fd5a8988e1574f427ac2d96351382e8de61fad14
namnguyenthanh <nam.nt0910@gmail.com>
2022-02-08 11:01:06 +0700

    fix bug BE

--- lib/data/request/send_offer_request.dart
+++ lib/data/request/send_offer_request.dart
@@ -4,39 +4,33 @@ part 'send_offer_request.g.dart';
 
 @JsonSerializable()
 class SendOfferRequest {
-  final int bcOfferId;
-  final int collateralId;
-  final String message;
-  final int duration;
+  final int cryptoCollateralId;
+  final String description;
+  final int durationQty;
   final int durationType;
   final num interestRate;
-  final String latestBlockchainTxn;
   final int liquidationThreshold;
   final num loanAmount;
   final num loanToValue;
   final int repaymentCycleType;
-  final int pawnShopPackageId;
-  final String repaymentToken;
-  final String supplyCurrency;
+  final String repaymentTokenSymbol;
+  final String txid;
   final String walletAddress;
 
-  SendOfferRequest({
-    required this.bcOfferId,
-    required this.collateralId,
-    required this.message,
-    required this.duration,
-    required this.durationType,
-    required this.interestRate,
-    required this.latestBlockchainTxn,
-    required this.loanAmount,
-    required this.repaymentCycleType,
-    required this.walletAddress,
-    required this.repaymentToken,
-    required this.supplyCurrency,
-    required this.liquidationThreshold,
-    required this.loanToValue,
-    required this.pawnShopPackageId,
-  });
+
+  SendOfferRequest(
+      this.cryptoCollateralId,
+      this.description,
+      this.durationQty,
+      this.durationType,
+      this.interestRate,
+      this.liquidationThreshold,
+      this.loanAmount,
+      this.loanToValue,
+      this.repaymentCycleType,
+      this.repaymentTokenSymbol,
+      this.txid,
+      this.walletAddress);
 
   factory SendOfferRequest.fromJson(Map<String, dynamic> json) =>
       _$SendOfferRequestFromJson(json);
