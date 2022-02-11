e4db530ed1201f500767de140fcb96459489f464
namnguyenthanh <nam.nt0910@gmail.com>
2022-02-11 15:48:37 +0700

    refactor code

--- lib/utils/constants/app_constants.dart
+++ lib/utils/constants/app_constants.dart
@@ -108,8 +108,8 @@ const int HARD_COLLECTION = 1;
 const int ERC721 = 0;
 const int ERC1155 = 1;
 const String BINANCE_SMART_CHAIN = 'Binance smart chain';
-const String ERC_721 = 'ERC_721';
-const String ERC_1155 = 'ERC_1155';
+const String ERC_721 = 'ERC-721';
+const String ERC_1155 = 'ERC-1155';
 const String NFT = 'NFT';
 
 // defi infomation
