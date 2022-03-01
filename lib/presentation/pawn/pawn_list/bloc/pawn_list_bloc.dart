e3afb7dca73ce2ae9c57bb32e83ae0f6d10fcec0
Doanhkull511a <doanhkull511a@gmail.com>
2022-03-01 14:04:08 +0700

    update logic filter list pawn shop

--- lib/presentation/pawn/pawn_list/bloc/pawn_list_bloc.dart
+++ lib/presentation/pawn/pawn_list/bloc/pawn_list_bloc.dart
@@ -1,8 +1,7 @@
-
 import 'package:Dfy/presentation/pawn/pawn_list/ui/dialog_filter.dart';
 
-class PawnListBloc{
-  TypeFilter? typeRating=TypeFilter.HIGH_TO_LOW;
-  TypeFilter? typeInterest=TypeFilter.HIGH_TO_LOW;
-  TypeFilter? typeSigned=TypeFilter.HIGH_TO_LOW;
+class PawnListBloc {
+  TypeFilter? typeRating = TypeFilter.HIGH_TO_LOW;
+  TypeFilter? typeInterest = TypeFilter.LOW_TO_HIGH;
+  TypeFilter? typeSigned = TypeFilter.HIGH_TO_LOW;
 }
