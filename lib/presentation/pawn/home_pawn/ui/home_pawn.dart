06d2c372bfa70857ed74444721953a033be5a39d
taquanghuy02032000 <taquanghuy0386356200@gmail.com>
2022-03-03 15:59:39 +0700

    Move home pawn to pawn folder.

--- lib/presentation/pawn/home_pawn/ui/home_pawn.dart
+++ lib/presentation/pawn/home_pawn/ui/home_pawn.dart
@@ -3,12 +3,13 @@ import 'package:Dfy/config/themes/app_theme.dart';
 import 'package:Dfy/data/exception/app_exception.dart';
 import 'package:Dfy/domain/model/nft_market_place.dart';
 import 'package:Dfy/generated/l10n.dart';
-import 'package:Dfy/presentation/home_pawn/bloc/home_pawn_cubit.dart';
-import 'package:Dfy/presentation/home_pawn/ui/components/banner_slide.dart';
-import 'package:Dfy/presentation/home_pawn/ui/components/list_item_horizontal.dart';
+
 import 'package:Dfy/presentation/market_place/ui/nft_item/ui/nft_item.dart';
 import 'package:Dfy/presentation/pawn/borrow_result/ui/borrow_result.dart';
 import 'package:Dfy/presentation/pawn/borrow_lend/ui/borrow_lend.dart';
+import 'package:Dfy/presentation/pawn/home_pawn/bloc/home_pawn_cubit.dart';
+import 'package:Dfy/presentation/pawn/home_pawn/ui/components/banner_slide.dart';
+import 'package:Dfy/presentation/pawn/home_pawn/ui/components/list_item_horizontal.dart';
 import 'package:Dfy/utils/constants/image_asset.dart';
 import 'package:Dfy/widgets/dialog/cupertino_loading.dart';
 import 'package:Dfy/widgets/dialog/modal_progress_hud.dart';
