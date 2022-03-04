11bc9d4ed4a8c9db54c283c968a9586e92e87045
Doanhkull511a <doanhkull511a@gmail.com>
2022-03-04 16:50:48 +0700

    fix ui detail offer

--- lib/presentation/offer_detail/ui/offer_detail_screen.dart
+++ lib/presentation/offer_detail/ui/offer_detail_screen.dart
@@ -545,12 +545,18 @@ class _OfferDetailScreenState extends State<OfferDetailScreen> {
               ),
             );
       },
-      child: Text(
-        status,
-        style: textNormalCustom(
-          AppTheme.getInstance().textThemeColor(),
-          16,
-          FontWeight.w700,
+      child: SizedBox(
+        width: 100.w,
+        child: Text(
+          status,
+          maxLines: 1,
+          style: textNormalCustom(
+            AppTheme.getInstance().textThemeColor(),
+            16,
+            FontWeight.w700,
+          ).copyWith(
+            overflow: TextOverflow.ellipsis,
+          ),
         ),
       ),
     );
