429e07eeb57938f1a71662df1751c05d6a2ce306
doanhdd <>
2022-03-10 11:48:01 +0700

    update ui

---
+++ lib/domain/model/pawn/collateral_result_model.dart
@@ -0,0 +1,23 @@
+class CollateralResultModel {
+  String? loadToken;
+  String? duration;
+  String? address;
+  String? rate;
+  String? iconLoadToken;
+  String? iconBorrower;
+  String? contracts;
+  String? iconCollateral;
+  String? collateral;
+
+  CollateralResultModel({
+    this.loadToken,
+    this.duration,
+    this.address,
+    this.rate,
+    this.iconLoadToken,
+    this.iconBorrower,
+    this.contracts,
+    this.iconCollateral,
+    this.collateral,
+  });
+}
