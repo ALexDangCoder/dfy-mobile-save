b195907b154733216eb88be346d14261ea2e9c23
doanhdd <>
2022-03-10 11:21:45 +0700

    update ui

--- lib/presentation/pawn/pawn_list/ui/pawn_shop_item.dart
+++ lib/presentation/pawn/pawn_list/ui/pawn_shop_item.dart
@@ -5,6 +5,7 @@ import 'package:Dfy/domain/model/pawn/token_model_pawn.dart';
 import 'package:Dfy/generated/l10n.dart';
 import 'package:Dfy/utils/constants/image_asset.dart';
 import 'package:Dfy/widgets/button/button.dart';
+import 'package:Dfy/widgets/common/info_popup.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -243,6 +244,7 @@ class PawnItem extends StatelessWidget {
                     ),
                   ),
                 ),
+                spaceW4,
                 Expanded(
                   child: RichText(
                     text: TextSpan(
@@ -316,6 +318,7 @@ class PawnItem extends StatelessWidget {
                     ),
                   ),
                 ),
+                spaceW4,
                 Expanded(
                   child: RichText(
                     text: TextSpan(
@@ -388,6 +391,7 @@ class PawnItem extends StatelessWidget {
                     ),
                   ),
                 ),
+                spaceW4,
                 Expanded(
                   child: Text(
                     availableLoan,
@@ -413,6 +417,7 @@ class PawnItem extends StatelessWidget {
                     ),
                   ),
                 ),
+                spaceW4,
                 Expanded(
                   child: Text(
                     interestRate,
@@ -441,10 +446,21 @@ class PawnItem extends StatelessWidget {
                   children: [
                     WidgetSpan(
                       alignment: PlaceholderAlignment.middle,
-                      child: Image.asset(
-                        ImageAssets.img_waning,
-                        height: 20.w,
-                        width: 20.w,
+                      child: GestureDetector(
+                        onTap: () {
+                          showDialog(
+                            context: context,
+                            builder: (_) => InfoPopup(
+                              name: S.current.duration,
+                              content: S.current.duration_more_content,
+                            ),
+                          );
+                        },
+                        child: Image.asset(
+                          ImageAssets.img_waning,
+                          height: 20.w,
+                          width: 20.w,
+                        ),
                       ),
                     ),
                   ],
