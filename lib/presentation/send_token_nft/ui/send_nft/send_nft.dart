ec080eceba7bce8e366b17d135ce74cfe127fce9
taquanghuy02032000 <taquanghuy0386356200@gmail.com>
2022-03-24 13:30:25 +0700

    pending

--- lib/presentation/send_token_nft/ui/send_nft/send_nft.dart
+++ lib/presentation/send_token_nft/ui/send_nft/send_nft.dart
@@ -60,6 +60,7 @@ class _SendNftState extends State<SendNft> {
     txtToAddressNft = TextEditingController();
     txtQuantity = TextEditingController();
     txtQuantity.text = '1';
+    print('standart ${widget.nftInfo.standard}');
   }
 
   @override
@@ -121,8 +122,7 @@ class _SendNftState extends State<SendNft> {
                     ),
                   ),
                 ).then(
-                  (value) => Navigator.of(context).pop(
-                  ),
+                  (value) => Navigator.of(context).pop(),
                 );
               }
             },
@@ -194,6 +194,8 @@ class _SendNftState extends State<SendNft> {
                                 height: 16.h,
                               ),
                               formAmountFtQuantity(
+                                readOnly: ((widget.nftInfo.standard ?? '') ==
+                                    ERC721.toString()) ? true : false,
                                 hintText: S.current.quantity,
                                 isAmount: true,
                                 isQuantity: true,
@@ -236,16 +238,12 @@ class _SendNftState extends State<SendNft> {
                                         nftId: widget.nftInfo.nftId ?? '',
                                         walletReceived: txtToAddressNft.text,
                                       );
-                                      showLoadSuccess(context).then(
-                                        (value) {
-                                          Navigator.of(context)
-                                              .pop();
-                                          Navigator.of(context)
-                                              .pop();
-                                          Navigator.of(context)
-                                              .pop(txtToAddressNft.text);
-                                        }
-                                      );
+                                      showLoadSuccess(context).then((value) {
+                                        Navigator.of(context).pop();
+                                        Navigator.of(context).pop();
+                                        Navigator.of(context)
+                                            .pop(txtToAddressNft.text);
+                                      });
                                     },
                                     onErrorSign: (context) {
                                       showLoadFail(context);
@@ -365,7 +363,9 @@ class _SendNftState extends State<SendNft> {
     required bool isQuantity,
     required String prefixImg,
     Function()? callBack,
+    bool? readOnly = false,
   }) {
+    print('readonyly $readOnly');
     return Container(
       height: 64.h,
       // width: 323.w,
@@ -383,6 +383,7 @@ class _SendNftState extends State<SendNft> {
               value: txtToAddressNft.text,
             );
           },
+          readOnly: (readOnly ?? false) ? true : false,
           keyboardType: TextInputType.number,
           textAlignVertical: TextAlignVertical.center,
           controller: txtQuantity,
